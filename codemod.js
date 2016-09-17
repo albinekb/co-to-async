b7d87169b51e5cfc389067645b7941369b834433
Albin Ekblom <albinekblom@gmail.com>
2016-09-17 20:47:20 +0200

    🤘

---
+++ codemod.js
@@ -0,0 +1,74 @@
+export default function transformer (file, api) {
+  const j = api.jscodeshift
+
+  const yieldToAwait = p => {
+    const node = p.node
+    const arg = node.argument
+    const n = j.awaitExpression(arg)
+    return n
+  }
+  const replaceCoWrap = p => {
+    const right = p.node.right
+    const args = right.arguments
+    if (args.length !== 1) throw new Error('Expected one argument')
+    const body = args[0]
+    return j.assignmentExpression(p.node.operator, p.node.left, body)
+  }
+
+  const removeCo = p => {
+    const node = p.node
+    const args = node.arguments
+    if (args.length !== 1) throw new Error('Expected one argument')
+    return args[0]
+  }
+
+  const generatorToAsync = p => {
+    const node = p.node
+    const {id, params, body, expression} = node
+    const n = j.functionExpression(id, params, body, false, expression)
+    n.async = true
+    return n
+  }
+
+  const generatorToAsyncDec = p => {
+    const node = p.node
+    const {id, params, body, expression} = node
+    const n = j.functionDeclaration(id, params, body, false, expression)
+    n.async = true
+    return n
+  }
+
+  // log filter
+  const log = id => p => { console.log(id, p.node); return true }
+
+  const root = j(file.source)
+
+  // yield -> await
+  root.find(j.YieldExpression)
+    .filter(log('Replace yield'))
+    .replaceWith(yieldToAwait)
+
+  // a = co.wrap(function * () {}) > a = function * () {}
+  root.find(j.AssignmentExpression, { right: { callee: { object: { name: 'co' }, property: { name: 'wrap' } } } })
+    .replaceWith(replaceCoWrap)
+
+  // co.wrap(...) -> ...
+  root.find(j.CallExpression, { callee: { object: { name: 'co' }, property: { name: 'wrap' } } })
+    .replaceWith(removeCo)
+
+  //  co(...) -> (async ...)()
+  root.find(j.CallExpression, { callee: { name: 'co' } })
+   .replaceWith(removeCo)
+   .replaceWith(p => j.callExpression(p.node, []))
+
+  // a = function * () -> a = async function ()
+  root.find(j.FunctionDeclaration, { generator: true })
+    .replaceWith(generatorToAsyncDec)
+
+  // function * () -> async function ()
+  root.find(j.FunctionExpression, { generator: true })
+    .filter(log('4'))
+    .replaceWith(generatorToAsync)
+
+  return root.toSource()
+}
